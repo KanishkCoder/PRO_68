84fa4030fd81a4a1edbaeabc686330db272970d0
KanishkCoder <76817700+KanishkCoder@users.noreply.github.com>
2021-06-01 16:40:28 +0530

    Update search.js

--- screens/search.js
+++ screens/search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class Search extends React.Component{
+export default class Facebook extends React.Component{
 
 
     render(){
